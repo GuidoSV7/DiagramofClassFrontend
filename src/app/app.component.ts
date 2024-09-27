67870f4f9cafa61719418a849ed128a03cfe5997
Guido Salazar Vargas <Guido.salazar.vargas7@gmail.com>
2024-09-26 21:02:15 -0400

    overlay v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,24 +15,60 @@ export class AppComponent implements OnInit, AfterViewInit {
   private paper: dia.Paper;
   private scroller: ui.PaperScroller;
 
+
+
   public ngOnInit(): void {
     const graph = this.graph = new dia.Graph({}, { cellNamespace: shapes });
 
     const paper = this.paper = new dia.Paper({
+        el: document.getElementById('paper'),
         model: graph,
         background: {
             color: '#F8F9FA',
         },
-        frozen: true,
+        width: 600,
+        height: 600,
         async: true,
-        sorting: dia.Paper.sorting.APPROX,
+
         cellViewNamespace: shapes
     });
+
+
+  // create stencil
+  const stencil = new ui.Stencil({
+    paper: paper,
+    width: 170,
+    height: 500,
+    layout: true,
+    dropAnimation: true
+  });
+  stencil.render();
+  document.getElementById('stencil')?.appendChild(stencil.el);
+
+  const elements = [
+    {
+        type: 'standard.Rectangle',
+        size: { width: 70, height: 50 },
+        attrs: {
+          body: {
+            fill: 'lightgray',
+            stroke: 'black',
+            strokeWidth: 2,
+            className: 'className',
+            attributes: 'attributes'
+          },
+        }
+    }
+
+
+  ];
+  stencil.load(elements);
+
 
     const scroller = this.scroller = new ui.PaperScroller({
         paper,
         autoResizePaper: true,
-        cursor: 'grab'
+
     });
 
     scroller.render();
@@ -55,10 +91,24 @@ export class AppComponent implements OnInit, AfterViewInit {
     console.log(this.graph.toJSON());
   }
 
+
+
+
   public ngAfterViewInit(): void {
-    const { scroller, paper, canvas } = this;
+    const { canvas } = this;
+    this.scroller = new ui.PaperScroller({
+      paper: this.paper,
+      autoResizePaper: true,
+      cursor: 'grab'
+    });
+    this.scroller.render();
     canvas.nativeElement.appendChild(this.scroller.el);
-    scroller.center();
-    paper.unfreeze();
+    this.scroller.center();
+
+    // Create halo
+    this.paper.on('cell:pointerup', (cellView) => {
+      new ui.Halo({ cellView: cellView }).render();
+    });
   }
+
 }
